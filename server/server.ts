69720ccd79946c9a73e059155237e6c6988f1d86
marksikaundi <emmanuelmark117@gmail.com>
2023-09-07 13:25:22 +0200

    server now running

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,9 @@
-import {app} from './app';
+import { app } from "./app";
+require("dotenv").config();
 
 // Path: server/server.ts
 
 // createServer() takes a requestListener callback function
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`);
+});
