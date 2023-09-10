ff818e5875dbac67c5422ccbc79c931f361d68e9
marksikaundi <emmanuelmark117@gmail.com>
2023-09-10 16:36:03 +0200

    done with error handling

--- server/utils/ErrorHandler.ts
+++ server/utils/ErrorHandler.ts
@@ -1,6 +1,16 @@
+class ErrorHandler extends Error {
+  statusCode: number;
+  message: string;
+  constructor(statusCode: number, message: string) {
+    super();
+    this.statusCode = statusCode;
+    this.message = message;
 
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
 
-
+module.exports = ErrorHandler;
 
 
 
