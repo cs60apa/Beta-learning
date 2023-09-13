cadd292948c4ec433ee4e6af912cb4c4b8cf1710
marksikaundi <emmanuelmark117@gmail.com>
2023-09-13 17:21:07 +0200

    done with error handling

--- server/middleware/error.ts
+++ server/middleware/error.ts
@@ -1,19 +1,36 @@
+import { NextFunction, Request, Response } from "express";
 import ErrorHandler from "../utils/ErrorHandler";
-module.exports = (err: ErrorHandler, req: any, res: any, next: any) => {
+module.exports = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
 
+  // Wrong Mongoose Object ID Error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+  // Duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message, 400);
+  }
+  // Wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = `JSON Web Token is invalid. Try Again!!!`;
+    err = new ErrorHandler(message, 400);
+  }
+  // JWT Expired Error
+  if (err.name === "TokenExpiredError") {
+    const message = `JSON Web Token is expired. Try Again!!!`;
+    err = new ErrorHandler(message, 400);
+  }
   res.status(err.statusCode).json({
     success: false,
-    error: err.message,
+    message: err.message,
   });
-}   
-
-
-
-
-
-
-
-
-
+};
