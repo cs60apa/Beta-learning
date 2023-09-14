2c1881865f6bac956ed559209ca7336890acf4e0
marksikaundi <emmanuelmark117@gmail.com>
2023-09-14 09:12:45 +0200

    timestamp 02:20:20

--- server/utils/sendMail.ts
+++ server/utils/sendMail.ts
@@ -1,39 +1,39 @@
-import nodemailer, { Transporter } from 'nodemailer';
-import ejs from 'ejs';
-import path from 'path';
+import nodemailer, { Transporter } from "nodemailer";
+import ejs from "ejs";
+import path from "path";
 
 interface IEmailOptions {
-    email: string;
-    subject: string;
-    template: string;
-    data: {[key:string]: any}
+  email: string;
+  subject: string;
+  template: string;
+  data: { [key: string]: any };
 }
 
 const sendMail = async (options: IEmailOptions): Promise<void> => {
-    const transporter: Transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        service: process.env.SMTP_SERVICE,
-        auth: {
-            user: process.env.SMTP_EMAIL,
-            pass: process.env.SMTP_PASSWORD
-        }
-    });
-    const {email, subject, template, data} = options;
+  const transporter: Transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    service: process.env.SMTP_SERVICE,
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+  const { email, subject, template, data } = options;
 
-    // render html template
-    const templatePath = path.join(__dirname, '../mails',template);
+  // render html template
+  const templatePath = path.join(__dirname, "../mails", template);
 
-    // render html template
-    const html:string = await ejs.renderFile(templatePath, data);
+  // render html template
+  const html: string = await ejs.renderFile(templatePath, data);
 
-    const mailOptions = {
-        from: process.env.SMTP_FROM,
-        to: email,
-        subject,
-        html
-    }
+  const mailOptions = {
+    from: process.env.SMTP_FROM,
+    to: email,
+    subject,
+    html,
+  };
 
-    await transporter.sendMail(mailOptions);
-}
+  await transporter.sendMail(mailOptions);
+};
 export default sendMail;
