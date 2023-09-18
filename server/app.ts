7e179de2fad9cdd87fae6e96d0495ca8f17aa5ab
marksikaundi <emmanuelmark117@gmail.com>
2023-09-18 17:48:12 +0200

    worked on user.route

--- server/app.ts
+++ server/app.ts
@@ -5,7 +5,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { ErrorMiddleware } from './middleware/error';
 import ErrorHandler from './utils/ErrorHandler';
-
+import userRouter from './routes/user.route';
 // body-parser is deprecated, so we use express.json() instead
 app.use(express.json({limit: '50mb'}));
 
@@ -17,6 +17,8 @@ app.use(cors({
     origin: process.env.ORIGIN,
 })); 
 
+// routes
+app.use("/api/v1", userRouter);
 
 // Testing api
 app.get('/test', (req: Request, res: Response, next: NextFunction) => {
---
+++ server/routes/user.route.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { registerUser, } from "../controllers/user.controller";
+
+const userRouter = express.Router();
+
+userRouter.post("/registerUser", registerUser);
+
+export default userRouter;
+
+
+
